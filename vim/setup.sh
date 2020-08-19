75f8d71730b282d88b1e954017906b92e1dd81ab
r08922195 <r08922195@linux4.csie.ntu.edu.tw>
2020-08-19 15:50:13 +0800

    Update vim setup

---
+++ vim/setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cp vimrc ~/.vimrc
+
+# install vim plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim -c :PlugInstall
