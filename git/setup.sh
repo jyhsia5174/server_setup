9bd9996810a51ec8481b8d77cb2f4a0f2f6c9afc
jyhsia <hsiajuiyang5174@gmail.com>
2020-08-19 15:55:04 +0800

    Update

---
+++ git/setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+git config --global user.name "jyhsia"
+git config --global user.email hsiajuiyang5174@gmail.com
+
+git config --global core.editor vim
--- ssh/setup.sh
+++ ssh/setup.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 ssh-keygen
+
+cp config ~/.ssh/
